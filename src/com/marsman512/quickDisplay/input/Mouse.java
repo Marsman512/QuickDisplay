7ced0f006d5482e1276a29861a7b170adf0c8d71
Marsman512 <marsman512@gmail.com>
2019-08-26 22:33:39 -0500

    Added some mouse button and scroll functionality

--- src/com/marsman512/quickDisplay/input/Mouse.java
+++ src/com/marsman512/quickDisplay/input/Mouse.java
@@ -1,11 +1,17 @@
 package com.marsman512.quickDisplay.input;
 
 import static org.lwjgl.glfw.GLFW.*;
-import org.lwjgl.glfw.GLFWCursorPosCallbackI;
 import com.marsman512.quickDisplay.DisplayManager;
+
+import org.lwjgl.glfw.GLFWCursorPosCallbackI;
+import org.lwjgl.glfw.GLFWMouseButtonCallbackI;
+import org.lwjgl.glfw.GLFWScrollCallbackI;
 
 import org.lwjgl.system.MemoryStack;
 import java.nio.DoubleBuffer;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 /**
  * A simple utility for managing mouse inputs
@@ -14,6 +20,24 @@ import java.nio.DoubleBuffer;
  */
 
 public class Mouse {
+	// Constants
+	
+	/**
+	 * Mouse button identifier
+	 */
+	public static final int
+		LEFT_BUTTON   = GLFW_MOUSE_BUTTON_LEFT   ,
+		RIGHT_BUTTON  = GLFW_MOUSE_BUTTON_RIGHT  ,
+		MIDDLE_BUTTON = GLFW_MOUSE_BUTTON_MIDDLE ;
+	
+	/**
+	 * 
+	 */
+	public static final int
+		BUTTON_PRESSED  = GLFW_PRESS  ,
+		BUTTON_RELEASED = GLFW_RELEASE;
+	
+	// Mouse movement
 	private static double mouseX, mouseY;
 	private static double lastX, lastY;
 	
@@ -22,6 +46,51 @@ public class Mouse {
 		mouseY = y;
 	};
 	
+	public static double getX() {
+		return mouseX;
+	}
+	
+	public static double getY() {
+		return mouseY;
+	}
+	
+	public static double getDX() {
+		return mouseX - lastX;
+	}
+	
+	public static double getDY() {
+		return mouseY - lastY;
+	}
+	
+	// Button events
+	private static final Queue<MouseButtonEvent> buttonEvents = new ArrayBlockingQueue<>(12);
+	
+	private static GLFWMouseButtonCallbackI mouseButtonCB = (long window, int button, int action, int mods) -> {
+		MouseButtonEvent buttonEvent = new MouseButtonEvent(button, action);
+		buttonEvents.add(buttonEvent);
+	};
+	
+	public static boolean hasButtonEvents() {
+		return !buttonEvents.isEmpty();
+	}
+	
+	public static MouseButtonEvent getButtonEvent() {
+		return buttonEvents.poll();
+	}
+	
+	// Scrolling
+	private static double currentScrollPos = 0.0d;
+	
+	private static GLFWScrollCallbackI scrollCB = (long window, double xoffset, double yoffset) -> {
+		currentScrollPos += yoffset;
+	};
+	
+	public static double getScrollPos() {
+		return currentScrollPos;
+	}
+	
+	// Internal methods
+	
 	/**
 	 * INTERNAL METHOD!
 	 * DO NOT USE!
@@ -29,6 +98,8 @@ public class Mouse {
 	public static void init() {
 		long windowID = DisplayManager.getWindowID();
 		glfwSetCursorPosCallback(windowID, cursorPosCB);
+		glfwSetMouseButtonCallback(windowID, mouseButtonCB);
+		glfwSetScrollCallback(windowID, scrollCB);
 		
 		try(MemoryStack stack = MemoryStack.stackPush()) {
 			DoubleBuffer x = stack.callocDouble(1);
@@ -49,23 +120,11 @@ public class Mouse {
 	 * DO NOT USE!
 	 */
 	public static void update() {
+		// Update mouse position variables
 		lastX = mouseX;
 		lastY = mouseY;
-	}
-	
-	public static double getX() {
-		return mouseX;
-	}
-	
-	public static double getY() {
-		return mouseY;
-	}
-	
-	public static double getDX() {
-		return mouseX - lastX;
-	}
-	
-	public static double getDY() {
-		return mouseY - lastY;
+		
+		// Clear event cache
+		buttonEvents.clear();
 	}
 }
---
+++ src/com/marsman512/quickDisplay/input/MouseButtonEvent.java
@@ -0,0 +1,11 @@
+package com.marsman512.quickDisplay.input;
+
+public class MouseButtonEvent {
+	public final int
+		button, type;
+	
+	public MouseButtonEvent(int button, int type) {
+		this.button = button;
+		this.type = type;
+	}
+}
